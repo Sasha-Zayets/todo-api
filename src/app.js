c0ff2a306c0f9b3a9a1aa1d1ae7aa795c1953d46
Sasha <sashazayets@webmaestro.com.ua>
2020-04-17 18:32:06 +0300

    header

--- src/app.js
+++ src/app.js
@@ -7,13 +7,11 @@ import routes from './routes';
 import { URL } from './constants/connect';
 
 const app = express();
-
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
-});
-
+  });	
 mongoose.connect(URL, {useNewUrlParser: true})
     .then(() => {
         console.log('connect database');
