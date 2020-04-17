a8e84b5cbe194ec048ac992ebb159f5a109d290d
Sasha <sashazayets@webmaestro.com.ua>
2020-04-17 18:01:30 +0300

    add test

--- src/app.js
+++ src/app.js
@@ -17,6 +17,14 @@ mongoose.connect(URL, {useNewUrlParser: true})
     .catch(error => console.log(error));
 
 app.use(cors());
+app.use(function (req, res, next) {
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
