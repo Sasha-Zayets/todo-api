7f4044a1be291cb1f96cc0675ba3b037de43595f
Sasha <sashazayets@webmaestro.com.ua>
2020-04-17 21:34:12 +0300

    add babel cli

--- src/app.js
+++ src/app.js
@@ -7,11 +7,7 @@ import routes from './routes';
 import { URL } from './constants/connect';
 
 const app = express();
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-  });	
+
 mongoose.connect(URL, {useNewUrlParser: true})
     .then(() => {
         console.log('connect database');
